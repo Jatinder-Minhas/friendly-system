51e1e3d0c9c14a755ca94602ee3d838874d6d6ba
Jatinder-Minhas <minhajai@sheridancollege.ca>
2020-08-16 09:39:27 -0400

    Added Main class to run the game

---
+++ src/ca/sheridancollege/project/Main.java
@@ -0,0 +1,18 @@
+package ca.sheridancollege.project;
+
+import ca.sheridancollege.project.game.BlackJack;
+
+/**
+ * This is the Main Class.
+ *
+ * @author Jatinderjitsingh Minhas jun 2020
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        BlackJack blackjack = new BlackJack("Black Jack");
+        System.out.println("Game Name: " + blackjack.getName());
+        blackjack.play();
+    }
+}

075b63abf672e30d9a7a9c6dd9ae887c0c55cf4e
bhatt <bhatt@192.168.0.46>
2020-08-16 11:21:10 -0400

    Improvement in test

---
+++ test/ca/sheridancollege/project/cards/HandTest.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project.cards;
+
+import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author bhatt
+ */
+public class HandTest {
+    
+           
+    
+    public HandTest() {
+        
+    }
+
+    /**
+     * Test of isBlackJack method, of class Hand. for Good Test
+     */
+    @Test
+    public void testIsBlackJackGood() {
+        System.out.println("isBlackJackGood");
+        int points = 21;
+        Hand instance = new Hand();
+        boolean expResult = true;
+        boolean result = instance.isBlackJack(points);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+    * Test of isBlackJack method, of class Hand. for Bad Test
+    */
+    @Test
+    public void testIsBlackJackBad() {
+        System.out.println("isBlackJackBad");
+        int points = 12;
+        Hand instance = new Hand();
+        boolean expResult = false;
+        boolean result = instance.isBlackJack(points);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+    * Test of isBlackJack method, of class Hand. for Boundary Test
+    */
+    @Test
+    public void testIsBlackJackBoundary() {
+        System.out.println("isBlackJackBoundary");
+        int points = 21;
+        Hand instance = new Hand();
+        boolean expResult = true;
+        boolean result = instance.isBlackJack(points);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testpoint()
+    {
+        System.out.println("point");
+        Card card = new Card(Card.RANK.FIVE, Card.SUIT.DIAMONDS);
+        Hand instance = new Hand();
+        int expResult = 5;
+        int result = instance.point(card);
+        assertEquals(expResult, result);
+    }
+}
